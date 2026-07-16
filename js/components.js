375b5903863f09cd5472ece8ed2ffbb9e9da1ab1
YousifH1221 <yousifhasan11@yahoo.com>
2026-07-16 19:25:07 -0400

    shared header and footer dynamically

---
+++ js/components.js
@@ -0,0 +1,26 @@
+// Load the website header
+fetch("components/header.html")
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(headerContent) {
+        document.getElementById("header-placeholder").innerHTML =
+            headerContent;
+    })
+    .catch(function(error) {
+        console.log("The header could not be loaded.", error);
+    });
+
+
+// Load the website footer
+fetch("components/footer.html")
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(footerContent) {
+        document.getElementById("footer-placeholder").innerHTML =
+            footerContent;
+    })
+    .catch(function(error) {
+        console.log("The footer could not be loaded.", error);
+    });
